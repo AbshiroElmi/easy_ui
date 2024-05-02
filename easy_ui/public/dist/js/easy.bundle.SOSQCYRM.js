f0bd53fe47768df21b723b3365fef437629b8e1d
AbshiroElmi <162014812+AbshiroElmi@users.noreply.github.com>
2024-05-02 11:36:08 +0300

    fixed the login theme

--- easy_ui/public/dist/js/easy.bundle.SOSQCYRM.js
+++ easy_ui/public/dist/js/easy.bundle.SOSQCYRM.js
@@ -12133,10 +12133,7 @@
     </div>
     <!-- /Search -->
     
-    
-
-    
-
+  
     <ul class="navbar-nav flex-row align-items-center ms-auto">
      
       <!-- Notification -->
@@ -12161,6 +12158,159 @@
             <div class="notification-list-header">
               <div class="header-items"></div>
               <div class="header-actions"></div>
+
+      
+      <!-- Search -->
+      <div class="navbar-nav align-items-center">
+        <div class="nav-item navbar-search-wrapper mb-0">
+        
+		  <form class="form-inline fill-width justify-content-end" role="search" onsubmit="return false;">
+			{% if (frappe.boot.read_only) { %}
+				<span class="indicator-pill yellow no-indicator-dot" title="{%= __("Your site is undergoing maintenance or being updated.") %}">
+					{%= __("Read Only Mode") %}
+				</span>
+			{% } %}
+			{% if (frappe.boot.user.impersonated_by) { %}
+				<span class="indicator-pill red no-indicator-dot" title="{%= __("You are impersonating as another user.") %}">
+					{%= __("Impersonating {0}", [frappe.boot.user.name]) %}
+				</span>
+			{% } %}
+			<div class="input-group search-bar text-muted hidden">
+				<input
+					id="navbar-search"
+					type="text"
+					class="form-control"
+					placeholder="{%= __('Search or type a command ({0})', [frappe.utils.is_mac() ? '\u2318 + G' : 'Ctrl + G']) %}"
+					aria-haspopup="true"
+				>
+				<span class="search-icon">
+					<svg class="icon icon-sm"><use href="#icon-search"></use></svg>
+				</span>
+			</div>
+		</form>
+        </div>
+      </div>
+      <!-- /Search -->
+      
+      
+
+      
+
+      <ul class="navbar-nav flex-row align-items-center ms-auto">
+       
+        <!-- Notification -->
+        <ul class="navbar-nav">
+					<li class="nav-item dropdown dropdown-notifications dropdown-mobile hidden">
+						<button
+							class="btn-reset nav-link notifications-icon text-muted"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false"
+						>
+							<span class="notifications-seen">
+								<span class="sr-only">{{ __("No new notifications") }}</span>
+								<svg class="es-icon icon-sm" style="stroke:none;"><use href="#es-line-notifications"></use></svg>
+							</span>
+							<span class="notifications-unseen">
+								<span class="sr-only">{{ __("You have unseen notifications") }}</span>
+								<svg class="es-icon icon-sm"><use href="#es-line-notifications-unseen"></use></svg>
+							</span>
+						</button>
+						<div class="dropdown-menu notifications-list dropdown-menu-right" role="menu">
+							<div class="notification-list-header">
+								<div class="header-items"></div>
+								<div class="header-actions"></div>
+							</div>
+							<div class="notification-list-body">
+								<div class="panel-notifications"></div>
+								<div class="panel-events"></div>
+								<div class="panel-changelog-feed"></div>
+							</div>
+						</div>
+					</li>
+					<li class="nav-item dropdown dropdown-message dropdown-mobile hidden">
+						<button
+							class="btn-reset nav-link notifications-icon text-muted"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="true"
+						>
+							<span>
+								<svg class="es-icon icon-sm"><use href="#es-line-chat-alt"></use></svg>
+							</span>
+						</button>
+					</li>
+					<li class="vertical-bar d-none d-sm-block"></li>
+					<li class="nav-item dropdown dropdown-help dropdown-mobile d-none d-lg-block">
+						<button
+							class="btn-reset nav-link"
+							data-toggle="dropdown"
+							aria-controls="toolbar-help"
+							aria-label="{{ __("Help Dropdown") }}"
+						>
+							<span>
+								{{ __("Help") }}
+								<svg class="es-icon icon-xs"><use href="#es-line-down"></use></svg>
+							</span>
+						</button>
+						<div class="dropdown-menu dropdown-menu-right" id="toolbar-help" role="menu">
+							<div id="help-links"></div>
+							<div class="dropdown-divider documentation-links"></div>
+							{% for item in navbar_settings.help_dropdown %}
+								{% if (!item.hidden) { %}
+									{% if (item.route) { %}
+										<a class="dropdown-item" href="{{ item.route }}">
+											{%= __(item.item_label) %}
+										</a>
+									{% } else if (item.action) { %}
+										<button class="btn-reset dropdown-item" onclick="return {{ item.action }}">
+											{%= __(item.item_label) %}
+										</button>
+									{% } else { %}
+										<div class="dropdown-divider"></div>
+									{% } %}
+								{% } %}
+							{% endfor %}
+						</div>
+					</li>
+					<li class="nav-item dropdown dropdown-navbar-user dropdown-mobile">
+					
+					
+					</li>
+				</ul>
+        <!--/ Notification -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- User -->
+        <li class="nav-item navbar-dropdown dropdown-user dropdown">
+          <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+            <div class="avatar avatar-online">
+				{{ avatar }}
+>>>>>>> 740c88c37ede407c4d60049c4be952a365b833d9
             </div>
             <div class="notification-list-body">
               <div class="panel-notifications"></div>
@@ -14467,4 +14617,4 @@
   };
 })();
 /*! For license information please see editor.js.LICENSE.txt */
-//# sourceMappingURL=easy.bundle.VDWHQ23Q.js.map
+//# sourceMappingURL=easy.bundle.SOSQCYRM.js.map
