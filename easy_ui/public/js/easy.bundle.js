0b39bf6d2d51a64735a483390f5b104fc29698eb
ldaahir <162431181+ldaahir@users.noreply.github.com>
2024-05-04 14:29:30 +0300

    drop down side bar

--- easy_ui/public/js/easy.bundle.js
+++ easy_ui/public/js/easy.bundle.js
@@ -1,10 +1,10 @@
 
 
-import "./theme-switcher.js";
-import "./frappe/ui/toolbar/navbar.html";
-import "./frappe/ui/page.html";
-import "./frappe/ui/pages.js";
-// import "./frappe/ui/toolbar/search.html";
-import "./frappe/views/workspace/workspaces.js";
-// import "./frappe/ui/toolbar/container.js"
-// import "./frappe/list/list_sidebar.js";
+// import "./theme-switcher.js";
+// import "./frappe/ui/toolbar/navbar.html";
+// import "./frappe/ui/page.html";
+// import "./frappe/ui/pages.js";
+// // import "./frappe/ui/toolbar/search.html";
+// import "./frappe/views/workspace/workspaces.js";
+// // import "./frappe/ui/toolbar/container.js"
+// // import "./frappe/list/list_sidebar.js";
